5e3ea5c345986f3a4fce0f08b5d00e10123cbc75
Ankit Singh <ankitsingh4154@gmail.com>
2021-08-19 11:25:15 +0530

    Binding IntelMotherboard bean with spring application context via ApplicationContextAware

--- src/com/compx/IntelMotherboard.java
+++ src/com/compx/IntelMotherboard.java
@@ -2,10 +2,16 @@ package com.compx;
 
 import java.util.List;
 
-public class IntelMotherboard implements Motherboard {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class IntelMotherboard implements Motherboard, ApplicationContextAware, BeanNameAware {
 
 	private String socketType;
 	private List<String> companies;
+	private ApplicationContext context;
 
 	@Override
 	public String getMotherBoard() {
@@ -26,6 +32,17 @@ public class IntelMotherboard implements Motherboard {
 
 	public void setCompanies(List<String> companies) {
 		this.companies = companies;
+	}
+
+	@Override
+	public void setBeanName(String name) {
+		System.out.println("Bean Name : "+ name);
+		
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext context) throws BeansException {
+		this.context = context;
 	}
 
 }
