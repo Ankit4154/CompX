7bdee171776256dec2aeadd4fff1e765b769abda
Ankit Singh <ankitsingh4154@gmail.com>
2021-09-01 12:02:45 +0530

    Around advice

--- src/com/compx/aspect/LoggingAspect.java
+++ src/com/compx/aspect/LoggingAspect.java
@@ -1,7 +1,9 @@
 package com.compx.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -26,7 +28,7 @@ public class LoggingAspect {
 		System.out.println("Advice run before for all Cache getter method calls(String return type)");
 	}
 	
-	@After("afterAllGetters()")
+	@After("allGetters()")
 	public void loggingAdviceForL3AfterCall() {
 		System.out.println("Advice run after all cache get method called using pointcut");
 	}
@@ -38,10 +40,21 @@ public class LoggingAspect {
 	}
 	// Using pointcut for same expression to be applied over different execution methods
 	@Pointcut("execution(public String com.compx.Cache.get*())")
-	public void afterAllGetters() { }
+	public void allGetters() { }
 	
 	// Pointcut for all methods of Cache
 	@Pointcut("within(com.compx.Cache)")
 	public void allCacheMethods() { }
 	
+	@Around("allGetters()")
+	public void aroundAdvice(ProceedingJoinPoint proceedingJoinPoint) {
+		System.out.println("Before advice");
+		try {
+			proceedingJoinPoint.proceed();
+		} catch (Throwable e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("After advice");
+	}
 }
